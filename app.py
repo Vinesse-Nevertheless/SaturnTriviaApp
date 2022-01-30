59adae5e2590027619bc5d2174efc82ec326ea52
Vanessa Reese <88684757+Vinesse-Nevertheless@users.noreply.github.com>
2022-01-29 21:33:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    codecs.open("index.html", "r", "utf-8")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
